ec0d48517628e9eec947661418c753927102a449
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-01 13:48:41 +1000

    User registration w/encryption & unique ids

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,37 +1,68 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const User = require("../models/user");
 
 const apiRouter = express.Router();
 
+function getFrontEndUser(user) {
+	//# Remove data that is unsafe for frontend
+	delete user.password;
+	return user;
+}
+
+//TODO: Webtoken authentication
+
 //# LEAVE
-apiRouter.get("/api/leave", (request,response) => { //# Fetch leave
-    console.log("Received request to fetch 'leave' items")
-    response.status(200).json({leave:"this will be data one day"})
-})
+apiRouter.get("/api/leave", (request, response) => {
+	//# Fetch leave
+	console.log("Received request to fetch 'leave' items");
+	response.status(200).json({ leave: "this will be data one day" });
+});
+
+//# LOGIN
+apiRouter.post("/api/login", async (request, response) => {
+	//TODO: User log in
+});
 
 //# USER REGISTRATION
-apiRouter.post("/api/registerUser", (request,response) => { //# Register user
-    console.log("Received request to register a new user")
-    
-    //TODO: Validate passed data
-    //TODO: Check if account with passed employee_id already exists
+apiRouter.post("/api/registerUser", async (request, response) => {
+	//# Register user
+	console.log("Received request to register a new user");
+    const employee_number = request.body.employee_number;
+    const password = request.body.password;
 
-    const userObj = {
-        employee_number:request.body.employee_number,
-        password:request.body.password
+	//TODO: Validate passed data (contains required fields)
+
+	const userExists = (await User.findOne({
+		employee_number: employee_number,
+	}))
+		? true
+        : false; //* Check if user with passed 'employee_number'
+        
+	if (!userExists) {
+		const userObj = {
+			employee_number: employee_number,
+			password: await bcrypt.hash(password, 10),
+			date: Date.now(),
+		};
+
+		const newUser = new User(userObj);
+		newUser.save().then((result) => {
+			console.log("new user data saved");
+			response.status(200).json(getFrontEndUser(userObj));
+		});
+	} else {
+        const error = `An account with the Employee Number '${employee_number}' already exists`;
+        response.status(401).json({error})
+        console.log(error);
     }
+});
 
-    const newUser = new User(userObj);
-    newUser.save().then(result => {
-        console.log("new user data saved")
-        response.status(200).json(userObj);
-    })
-})
-
-apiRouter.get("/api/*", (request,response) => { //# Catch all
-    response.status(404).json({error:"invalid api call (bad address)"})
-})
+apiRouter.get("/api/*", (request, response) => {
+	//# Catch all
+	response.status(404).json({ error: "invalid api call (bad address)" });
+});
 
 module.exports = apiRouter;
