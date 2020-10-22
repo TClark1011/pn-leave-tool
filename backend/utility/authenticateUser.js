6e55b1df2c0e745d008d0386a841dee02d9257db
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-22 13:07:44 +1100

    Added doc comments

--- backend/utility/authenticateUser.js
+++ backend/utility/authenticateUser.js
@@ -1,8 +1,16 @@
 const bcrypt = require("bcrypt");
 
-async function authenticateUser(user, receivedPassword) {
+/**
+ * Authenticate user by comparing user provided password with stored password
+ * @param {Object} storedUser - The user record taken from the database
+ * @param {string} receivedPassword - The password provided by the user
+ * @returns {boolean} Whether or not 'receivedPassword' matches (according to bcrypt) with the password field of 'storedUser'
+ */
+async function authenticateUser(storedUser, receivedPassword) {
 	//# Returns true/false depending on if 'receivedPassword' matches the user's encrypted password
-	return (await bcrypt.compare(receivedPassword, user.password)) ? true : false;
+	return (await bcrypt.compare(receivedPassword, storedUser.password))
+		? true
+		: false;
 }
 
 module.exports = authenticateUser;
