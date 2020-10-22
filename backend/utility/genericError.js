f38306caae79053663e3f7090e6aa6fb6f60594d
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-22 13:08:17 +1100

    Added doc comments

--- backend/utility/genericError.js
+++ backend/utility/genericError.js
@@ -1,5 +1,9 @@
+/**
+ * Generates a generic error message to return to the frontend
+ * @param {string} action - The activity that was being attempted when the error ocurred (in past-tense verb form)
+ * @returns {string} A generated string to return to frontend as an error message
+ */
 function genericError(action) {
-	//# Generate a generic error message
 	return `An error occurred while attempting to ${action}. Please restart the application or try again later.`;
 }
 
