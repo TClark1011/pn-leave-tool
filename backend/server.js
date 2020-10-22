0001ee26b29f290b1156899bdde6faa993cf7910
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-22 12:36:07 +1100

    removed old commented code

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-
-// //# Request routes are handled in external files
-// const apiRouter = require("./routes/api");
-// const userRouter = require("./routes/userRoutes");
-
-// const app = express();
-// app.use(express.json());
-
-// app.use("/api/users", userRouter);
-// app.use(apiRouter);
-
-// app.get("/api/*", (request, response) => {
-// 	//# Catch all if a request is not handled by any other route
-// 	//# It is important for this to come last so that it only catches requests that are not handled by any other route.
-// 	response.status(404).json({ error: "invalid api call (bad address)" });
-// });
-
-// app.get("/*", (request, response) => {
-// 	//# Returns any non-api routed request to the root.
-// 	//# All navigation is done via dom-routing, so this should only be triggered when a user first navigates to the application or when they refresh their browser
-// 	response.status(404).redirect("/");
-// });
-
-// app.use(express.static("build"));
-
 const app = require("./app");
 
 const PORT = process.env.BACKEND_PORT || process.env.PORT || 3001;
