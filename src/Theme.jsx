5d3c04211d404a832135c7a96e3bc9040c63d508
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-10-27 08:41:49 +1100

    Changed colour palette

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -1,28 +1,28 @@
-import { createMuiTheme }  from '@material-ui/core/styles'
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
-    palette: {
-        primary:{main:"#004B8D"},
-        secondary:{main:"#F2A900"},
-    },
-    typography:{
-        fontFamily:[
-            'Roboto',
-            'sans-serif'
-        ].join(','),
-        fontSize:16,
-        h1:{
-            fontSize:"3em",
-            fontWeight:600,
-        },
-        h2: {
-            fontSize:"2em",
-            fontWeight:500,
-        },
-    }
-})
+	palette: {
+		type: "light",
+		primary: { main: "#004B8D", light: "#007bc3", dark: "#002d6b" },
+		secondary: { main: "#F2A900" },
+	},
+	typography: {
+		fontFamily: ["Roboto", "sans-serif"].join(","),
+		fontSize: 16,
+		h1: {
+			fontSize: "3em",
+			fontWeight: 600,
+			color: "#212121",
+		},
+		h2: {
+			fontSize: "2em",
+			fontWeight: 500,
+			color: "#212121",
+		},
+	},
+});
 
 //# NOTES
 //* Page Titles (eg; 'Login') use h2
 
-export default theme
+export default theme;
