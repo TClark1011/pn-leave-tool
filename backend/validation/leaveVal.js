fb31f6feeae09bf7d92e0d69f0b1f13bb112056f
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-11-12 08:17:18 +1100

    Fixed end date validation

--- backend/validation/leaveVal.js
+++ backend/validation/leaveVal.js
@@ -2,13 +2,22 @@ const yup = require("yup");
 
 const user = require("./fields/employee_number");
 
-const addDays = require("date-fns/addDays");
+const addWeeks = require("date-fns/addWeeks");
+const startOfToday = require("date-fns/startOfToday");
 
 module.exports = yup.object({
 	user,
 	dates: yup.object({
-		start: yup.date().required().min(addDays(new Date(), 13)),
-		end: yup.date().required(),
+		start: yup.date().required().min(addWeeks(startOfToday(), 2)),
+		end: yup
+			.date()
+			.required()
+			.test(
+				"ahead of start",
+				"end date must be later than start date",
+				function (value) {
+					return new Date(value) > new Date(this.resolve(yup.ref("start")));
+				}
+			),
 	}),
-	//TODO: Make end's 'min' value work
 });
