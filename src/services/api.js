0134cb6c9d06bbf3a6d0755467d456a97683e553
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-11-18 10:43:19 +1100

    Added register

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,11 @@ import axios from "axios";
 export const login = async (data) => {
 	return axios.post("/api/users/login", data);
 };
+
+export const register = async (data) => {
+	try {
+		return await axios.post("api/users/register", data);
+	} catch (err) {
+		return err;
+	}
+};
