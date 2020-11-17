0ccb30c2ae25d82e465beb2cc2ada272f1b09ba0
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-11-18 10:43:47 +1100

    Added function to format field labels nicely

---
+++ src/services/formatLabel.js
@@ -0,0 +1,28 @@
+//#Formats a code label like "employee_number" or "employeeNumber" into the format "Employee Number"
+
+module.exports = (string) => {
+	string = string.substr(0, 1).toUpperCase() + string.substr(1);
+	string = string.replace(/_/g, " ");
+	var lastCharCap = false;
+	for (let i = 1; i < string.length; i++) {
+		if (string.charAt(i - 1) === " ") {
+			string =
+				string.substr(0, i) +
+				string.charAt(i).toUpperCase() +
+				string.substr(i + 1);
+		}
+		const char = string.charAt(i);
+		if (
+			/[a-zA-Z]/.test(char) &&
+			char.charCodeAt(0) === char.toUpperCase().charCodeAt(0)
+		) {
+			if (!lastCharCap) {
+				lastCharCap = true;
+				string = string.substr(0, i) + " " + string.substr(i);
+			}
+		} else {
+			lastCharCap = false;
+		}
+	}
+	return string;
+};
