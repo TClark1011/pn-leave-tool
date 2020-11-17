27c13f8830043a53c939b21773ecb5191b835c9e
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-11-17 13:23:01 +1100

    Added custom validation messages

--- src/validation/loginVal.js
+++ src/validation/loginVal.js
@@ -3,6 +3,8 @@ const yup = require("yup");
 const loginSchema = require("./schemas/loginSchema");
 
 module.exports = yup.object({
-	employee_number: loginSchema.employee_number.required(),
-	password: loginSchema.password.required(),
+	employee_number: loginSchema.employee_number.required(
+		"Employee Number is a required field"
+	),
+	password: loginSchema.password.required("Password is a required field"),
 });
