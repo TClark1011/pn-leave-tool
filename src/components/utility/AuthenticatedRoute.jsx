92119d3f11d5ca39c928e78600d850e997811d22
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-11-19 13:44:24 +1100

    Refactored 'AuthenticatedRoute' to use 'AuthenticatedItem'

---
+++ src/components/utility/AuthenticatedRoute.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Route, withRouter } from "react-router-dom";
+
+import AuthenticatedItem from "./AuthenticatedItem";
+
+function AuthenticatedRoute(props) {
+	return (
+		<AuthenticatedItem elseRender={props.history.push("/login?redir")}>
+			<Route {...props}>{props.children}</Route>
+		</AuthenticatedItem>
+	);
+}
+
+export default withRouter(AuthenticatedRoute);
