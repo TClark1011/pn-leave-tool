c3725b84fb56f8516727e87fef10baf39a143f8d
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-11-19 13:54:59 +1100

    add 'elseRun' prop

--- src/components/utility/AuthenticatedItem.jsx
+++ src/components/utility/AuthenticatedItem.jsx
@@ -3,10 +3,13 @@ import React, { useContext } from "react";
 
 import UserContext from "./UserContext";
 
-function AuthenticatedItem({ elseRender, children, ...props }) {
+function AuthenticatedItem({ elseRender, elseRun, children, ...props }) {
 	const { user, setUser } = useContext(UserContext);
 	if (user) {
 		return <>{children}</>;
+	}
+	if (typeof elseRun === "function") {
+		elseRun();
 	}
 	return elseRender || null;
 }
