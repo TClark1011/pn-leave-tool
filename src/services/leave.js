f6141ff83f18e9b4290340060adca75afc026dd2
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-08 10:09:05 +1100

    changed urls for new backend

--- src/services/leave.js
+++ src/services/leave.js
@@ -1,6 +1,6 @@
 import axios from "../utils/axiosInstance";
 
-export const submitLeave = (data) => axios.post("/api/leave/request", data);
+export const submitLeave = (data) => axios.post("/leave/request", data);
 
 export const getLeave = (employee_number) =>
-	axios.get(`/api/leave/view/?user=${employee_number}`);
+	axios.get(`/leave/view/?user=${employee_number}`);
