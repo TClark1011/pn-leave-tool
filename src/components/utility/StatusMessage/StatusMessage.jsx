d409873926d8d4fc47d557cabdaf48da3f72add6
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-19 13:05:56 +1100

    Use 'Link instead of 'a'

--- src/components/utility/StatusMessage/StatusMessage.jsx
+++ src/components/utility/StatusMessage/StatusMessage.jsx
@@ -3,6 +3,8 @@ import "./StatusMessage.scss";
 import React from "react";
 
 import classnames from "classnames";
+import { Box, Link } from "@material-ui/core";
+import BodyText from "../BodyText";
 
 function StatusMessage({ tone, className, border, children, ...props }) {
 	const classes = classnames(
@@ -15,9 +17,19 @@ function StatusMessage({ tone, className, border, children, ...props }) {
 
 	if (children) {
 		return (
-			<div className={classes} {...props}>
+			<Box className={classes} {...props}>
 				{children}
-			</div>
+				{tone === "negative" && (
+					<>
+						<br />
+						<br />
+						You can contact us at{" "}
+						<Link href={`mailto:${process.env.REACT_APP_SUPPORT_EMAIL}`}>
+							{process.env.REACT_APP_SUPPORT_EMAIL}
+						</Link>
+					</>
+				)}
+			</Box>
 		);
 	} else {
 		return "";
