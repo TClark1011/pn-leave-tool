dd6351070d68fa48ff2dba1f8b8a32c9ed4632ee
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-27 10:08:10 +1100

    Test if 'Content' element was rendered

--- src/__Test__/App.spec.jsx
+++ src/__Test__/App.spec.jsx
@@ -1,5 +1,16 @@
-it("Renders the root element", () => {
-	render(<App />);
-	const rootEl = document.getElementById("__App-Root__");
-	expect(rootEl).toBeInTheDocument();
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "../App";
+
+describe("Root element rendering", () => {
+	it("Renders the root element", () => {
+		render(<App />);
+		const rootEl = document.getElementById("App");
+		expect(rootEl).toBeTruthy();
+	});
+	it("Renders the content element", () => {
+		render(<App />);
+		const rootEl = document.getElementById("Content");
+		expect(rootEl).toBeTruthy();
+	});
 });
