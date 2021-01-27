cc471ec812635b6a7e7ea3839bd941d398d1fca7
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-28 08:52:16 +1100

    added jsdoc to 'setMultipleProperties'

--- src/styles/config/utilities.js
+++ src/styles/config/utilities.js
@@ -1,3 +1,10 @@
+/**
+ * Set multiple css properties to a single value
+ *
+ * @param {string} value The css value to apply to each property
+ * @param {string[]} properties An array of css properties to apply 'value' to
+ * @returns {string} A css sting that can be used in styled-components
+ */
 export const setMultipleProperties = (value, properties) => {
 	const cssArray = [];
 	properties.map((item) => cssArray.push(`${item}:${value};`));
