3cf3d31dcfcaeb9a5bc4c3968e7908370b07c898
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-28 08:36:57 +1100

    removed custom spacing from material-ui theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -24,21 +24,6 @@ const theme = createMuiTheme({
 			color: "#212121",
 		},
 	},
-	spacing: [
-		"0.25rem",
-		"0.5rem",
-		"0.75rem",
-		"1rem",
-		"1.5rem",
-		"2rem",
-		"3rem",
-		"4rem",
-		"6rem",
-		"8rem",
-		"12rem",
-		"16rem",
-		"24rem",
-	],
 });
 
 //TODO: Pull values from scss using solution from here: https://github.com/css-modules/css-modules/issues/86
