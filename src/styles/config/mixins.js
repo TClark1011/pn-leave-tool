8da63ae32f4c4db966b9a42c3669643128f2be54
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-28 13:40:55 +1100

    Added extra options to 'flexWrap' mixin

--- src/styles/config/mixins.js
+++ src/styles/config/mixins.js
@@ -4,10 +4,14 @@ export const flexWrapperMixin = ({
 	isInline = false,
 	justify = "flex-start",
 	align = "flex-start",
+	wrap = "nowrap",
+	direction = "row",
 }) => css`
 	display: ${isInline ? "inline-flex" : "flex"};
 	justify-content: ${justify};
 	align-items: ${align};
+	flex-wrap: ${wrap};
+	flex-direction: ${direction};
 `;
 
 export const yMarginsMixin = (value) => css`
