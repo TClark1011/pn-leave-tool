713f6f1ce3fed0e60de2dc8392fc8e12ee8d8c48
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-28 12:19:17 +1100

    fixed flexWrapper mixin syntax

--- src/styles/config/mixins.js
+++ src/styles/config/mixins.js
@@ -5,5 +5,25 @@ export const flexWrapperMixin = ({
 }) => `
 	display: ${isInline ? "inline-flex" : "flex"};
 	justify-content:${justify};
-	align-items:${align}	
+	align-items:${align};
+`;
+
+export const yMarginsMixin = (value) => `
+	margin-top:${value};
+	margin-bottom:${value};
+`;
+
+export const xMarginsMixin = (value) => `
+	margin-left:${value};
+	margin-right:${value};
+`;
+
+export const xPaddingMixin = (value) => `
+	padding-left:${value};
+	padding-right:${value};
+`;
+
+export const yPaddingMixin = (value) => `
+	padding-top:${value};
+	padding-bottom:${value};
 `;
