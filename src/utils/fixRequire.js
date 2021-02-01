959213531631f5c7d53f3331681e225c98b9ee01
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-02 08:48:08 +1100

    deleted unused 'fixRequire' file

--- src/utils/fixRequire.js
+++
@@ -1,9 +0,0 @@
-//# Fix an issue with requiring functions from 'date-fns' v2
-
-module.exports = (requiredItem, expectedType) => {
-	expectedType = expectedType || "function";
-	if (typeof requiredItem === expectedType) {
-		return requiredItem;
-	}
-	return requiredItem.default;
-};
