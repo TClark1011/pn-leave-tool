b845cffb7e6989e10ba7216d1e77050a99d8aada
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-02 08:24:45 +1100

    Changed 'App' stylesheet to use theme selector

--- src/App.styles.js
+++ src/App.styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { flexWrapperMixin } from "./styles/config/mixins";
 import { bottomNavHeight } from "./styles/config/styleVars";
+import { spacing } from "./styles/config/themeSelectors";
 
 export const AppRoot = styled.div`
 	${flexWrapperMixin({ justify: "center", align: "center" })}
@@ -8,5 +9,5 @@ export const AppRoot = styled.div`
 `;
 
 export const ContentContainer = styled.div`
-	padding: ${(p) => p.theme.spacing(4)}px;
+	padding: ${spacing(4)}px;
 `;
