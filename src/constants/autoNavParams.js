1636568f0f2f16a0eddf35486813e6ee65964fb5
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-02 09:00:32 +1100

    reformatted comment in 'autoNavParams.js'

--- src/constants/autoNavParams.js
+++ src/constants/autoNavParams.js
@@ -1,9 +1,7 @@
 //# This file contains addresses used when automatically navigating the user eg; landing page redirect
 
-/**
- * Where to redirect user after they log in
- */
 export const loginRedir = "/request";
+//? Where to redirect user after they log in
 
 export const loggedInCookie = "loggedInPreviously=true";
 
@@ -14,7 +12,8 @@ export const existingClientLandingRedir = "/login";
 /**
  * Where to redirect the user from the landing page
  *
- * Redirect user to login if they have ever signed in before, otherwise redirect them to registration
+ * Redirect user to login if they have ever signed in before,
+ * otherwise redirect them to registration
  */
 export const getLandingRedir = () =>
 	document.cookie.includes(loggedInCookie)
