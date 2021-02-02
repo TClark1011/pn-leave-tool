d81c9950a33a02d29ea8dc39a45cca9ef6ebe038
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-03 10:09:07 +1100

    implemented jsdocs into 'AuthHelperText'

--- src/components/AuthForms/AuthHelperText.jsx
+++ src/components/AuthForms/AuthHelperText.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { AuthHelperTextWrapper } from "./AuthForms.styles";
 
-function AuthHelperText({ ...props }) {
+/**
+ * Help text that appears in both the login and registration
+ * form.
+ *
+ * @param {object} props The component props
+ * @param {ReactNode} props.children The component children
+ * @returns {ReactNode} The component's rendered elements
+ */
+const AuthHelperText = ({ ...props }) => {
 	return (
 		<AuthHelperTextWrapper {...props}>
 			If you have never used this tool before, you must register an account. You
 			cannot sign in to the tool with any existing Pacific National accounts.
 		</AuthHelperTextWrapper>
 	);
-}
+};
 
 export default AuthHelperText;
