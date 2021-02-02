096ee1a38f3bec38dbef0f7cd87f539a9514f36d
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-03 09:45:10 +1100

    export 'VALIDATE_EMAIL' var from 'env.js'

--- src/constants/env.js
+++ src/constants/env.js
@@ -3,6 +3,8 @@
  * This allows for nicer syntax and for jest to mock env variables.
  */
 
+import toBoolean from "to-boolean";
+
 export const backendDevPort = process.env.REACT_APP_BACKEND_DEV_PORT;
 //? The port used by the backend during development
 
@@ -11,3 +13,7 @@ export const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 export const supportEmail = process.env.REACT_APP_SUPPORT_EMAIL;
 //? The support email address
+
+export const validateEmail =
+	!process.env.REACT_APP_VALIDATE_EMAIL ||
+	toBoolean(process.env.REACT_APP_VALIDATE_EMAIL);
