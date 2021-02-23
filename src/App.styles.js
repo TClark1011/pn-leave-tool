70591cb7c3f5c3f2f856c2624566581064ff1b0e
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-24 08:06:55 +1100

    Fixed width/height errors

--- src/App.styles.js
+++ src/App.styles.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 import { flexWrapperMixin } from "./styles/config/mixins";
-import { bottomNavHeight } from "./styles/config/styleVars";
 import { spacing } from "./styles/config/themeSelectors";
 
 export const AppRoot = styled.div`
-	${flexWrapperMixin({ justify: "center", align: "center" })}
-	padding-bottom:${(p) => p.theme.spacing(4) + bottomNavHeight}px;
+	${flexWrapperMixin({
+		justify: "center",
+		align: "center",
+	})}
 `;
 
 export const ContentContainer = styled.div`
-	padding: ${spacing(4)}px;
+	padding: ${spacing(4)}px 0;
 `;
