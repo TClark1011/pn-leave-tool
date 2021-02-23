789da48ce9d79ce75d2d1041001b9fab13f36454
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-24 08:46:23 +1100

    created 'userFixture' for testing

---
+++ src/fixtures/userFixture.js
@@ -0,0 +1,17 @@
+export default {
+	_id: "6018ba539e96d15ae07ada5b",
+	date_created: "2021-02-02T02:16:57.432Z",
+
+	depot: { _id: "6018c448ad35c900174e0455", name: "Test Depot" },
+	email: "thomassiclark@gmail.com",
+	employee_number: "111111",
+	name: "Test User",
+	passwordReset: {
+		isResettingPassword: false,
+		passwordResetKey: "",
+		passwordResetKeyExpires: "1970-01-01T00:00:00.000Z",
+	},
+	token:
+		"eyJhbGciOiJIUzI1NiJ9.MTExMTEx.yqxhTwjyNhitYxnWVN34rJdTsGBVQ0Akq3AwvTrEI5g",
+	verified: true,
+};
